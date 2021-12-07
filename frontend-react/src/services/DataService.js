76190237725eaccbcf94eaa4707063cc5067fb55
sytseng <shihyi_tseng@g.harvard.edu>
2021-12-07 00:07:57 -0500

    Add Predict_prefix to DataService

--- frontend-react/src/services/DataService.js
+++ frontend-react/src/services/DataService.js
@@ -6,14 +6,15 @@ const DataService = {
     Init: function () {
         // Any application initialization logic comes here
     },
-    GetLeaderboard: async function () {
-        return await axios.get(BASE_API_URL + "/leaderboard");
-    },
-    GetCurrentmodel: async function () {
-        return await axios.get(BASE_API_URL + "/best_model");
-    },
     Predict: async function (formData) {
         return await axios.post(BASE_API_URL + "/predict", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    },
+    Predict_prefix: async function (formData) {
+        return await axios.post(BASE_API_URL + "/predict_prefix", formData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
