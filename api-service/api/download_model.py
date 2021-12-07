645c2250c73761c5df91fb4e9a76e11981ea22a7
sytseng <shihyi_tseng@g.harvard.edu>
2021-12-07 00:05:36 -0500

    Add function to download prefix model

--- api-service/api/download_model.py
+++ api-service/api/download_model.py
@@ -6,10 +6,14 @@ bucket_name = "ac215-project"
 persistent_folder = "/persistent"
 cgp_model_folder_transformer = "transformer_model"
 transformer_model_path = os.path.join(persistent_folder, cgp_model_folder_transformer)
+cgp_model_folder_prefix = "prefix_model"
+prefix_model_path = os.path.join(persistent_folder, cgp_model_folder_prefix)
 image_path = os.path.join(persistent_folder, "image")
 
 if not os.path.exists(transformer_model_path):
     os.mkdir(transformer_model_path)
+if not os.path.exists(prefix_model_path):
+    os.mkdir(prefix_model_path)
 if not os.path.exists(image_path):
     os.mkdir(image_path)
 
@@ -21,30 +25,53 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     blob.download_to_filename(destination_file_name)
 
 
-def download_transformer_model():
-    vectorization_file = os.path.join(cgp_model_folder_transformer,"vectorization_weights.pkl")
-    encoder_file = os.path.join(cgp_model_folder_transformer,"encoder.h5")
-    decoder_file = os.path.join(cgp_model_folder_transformer,"decoder.h5")
+def download_test_image():
     test_image_file = "test_image.jpeg"
-
-    if not os.path.exists(os.path.join(persistent_folder, vectorization_file)):
-        print('Downloading vectorization...')
-        download_blob(bucket_name, vectorization_file, os.path.join(persistent_folder, vectorization_file))
-
-    if not os.path.exists(os.path.join(persistent_folder, encoder_file)):
-        print('Downloading encoder weights...')
-        download_blob(bucket_name, encoder_file, os.path.join(persistent_folder, encoder_file))
-
-    if not os.path.exists(os.path.join(persistent_folder, decoder_file)):
-        print('Downloading decoder weights...')
-        download_blob(bucket_name, decoder_file, os.path.join(persistent_folder, decoder_file))
 
     if not os.path.exists(os.path.join(image_path, test_image_file)):
         print('Downloading test image...')
         download_blob(bucket_name, test_image_file, os.path.join(image_path, test_image_file))
 
 
+def download_transformer_model():
+    vectorization_file = os.path.join(cgp_model_folder_transformer,"vectorization_weights.pkl")
+    encoder_file = os.path.join(cgp_model_folder_transformer,"encoder.h5")
+    decoder_file = os.path.join(cgp_model_folder_transformer,"decoder.h5")
+
+    if not os.path.exists(os.path.join(persistent_folder, vectorization_file)):
+        print('Downloading transformer_model vectorization...')
+        download_blob(bucket_name, vectorization_file, os.path.join(persistent_folder, vectorization_file))
+
+    if not os.path.exists(os.path.join(persistent_folder, encoder_file)):
+        print('Downloading transformer_model encoder weights...')
+        download_blob(bucket_name, encoder_file, os.path.join(persistent_folder, encoder_file))
+
+    if not os.path.exists(os.path.join(persistent_folder, decoder_file)):
+        print('Downloading transformer_model decoder weights...')
+        download_blob(bucket_name, decoder_file, os.path.join(persistent_folder, decoder_file))
+
+    print('Done!')
+
+def download_prefix_model():
+    vectorization_file = os.path.join(cgp_model_folder_prefix,"vectorization_weights.pkl")
+    encoder_file = os.path.join(cgp_model_folder_prefix,"encoder.h5")
+    decoder_file = os.path.join(cgp_model_folder_prefix,"decoder.h5")
+
+    if not os.path.exists(os.path.join(persistent_folder, vectorization_file)):
+        print('Downloading prefix_model vectorization...')
+        download_blob(bucket_name, vectorization_file, os.path.join(persistent_folder, vectorization_file))
+
+    if not os.path.exists(os.path.join(persistent_folder, encoder_file)):
+        print('Downloading prefix_model encoder weights...')
+        download_blob(bucket_name, encoder_file, os.path.join(persistent_folder, encoder_file))
+
+    if not os.path.exists(os.path.join(persistent_folder, decoder_file)):
+        print('Downloading prefix_model decoder weights...')
+        download_blob(bucket_name, decoder_file, os.path.join(persistent_folder, decoder_file))
+
     print('Done!')
 
 if __name__ == "__main__":
+    download_test_image()
     download_transformer_model()
+    download_prefix_model()
